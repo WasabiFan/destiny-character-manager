5bdbeb13f56596b8b9bf43900f228f276a8c0eff
Wasabi Fan <wasabifan@outlook.com>
2015-07-09 16:21:28 -0700

    Add basic config manager

---
+++ ManagerConsole/ManagerConsole/config-manager.js
@@ -0,0 +1,36 @@
+﻿var fs = require('fs');
+
+var configFilePath = 'character-conf.json';
+var defaultConfigObj = {
+    bungieCookie: ''
+}
+
+var configCache;
+
+exports.load = function () {
+    try {
+        var configStr = fs.readFileSync(configFilePath);
+        configCache = JSON.parse(configStr);
+    }
+    catch (e) {
+        configCache = defaultConfigObj;
+        return;
+    }
+};
+
+exports.save = function () {
+    if (configCache == undefined)
+        configCache = defaultConfigObj;
+
+    fs.writeFileSync(configFilePath, JSON.stringify(configCache));
+}
+
+exports.get = function (name) {
+    return configCache[name];
+}
+
+exports.set = function (name, property) {
+    return configCache[name] = property;
+}
+
+exports.load();
