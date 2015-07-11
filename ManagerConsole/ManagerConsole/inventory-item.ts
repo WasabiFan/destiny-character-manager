bb50d3f0eeb8a69944e4bbb824a90eb7159d613e
Wasabi Fan <wasabifan@outlook.com>
2015-07-10 23:04:25 -0700

    Add inventory classes and parser skeleton

---
+++ ManagerConsole/ManagerConsole/inventory-item.ts
@@ -0,0 +1,99 @@
+﻿export class InventoryItem {
+    public name: string;
+    public tier: InventoryItemTier;
+    public type: InventoryItemType;
+    public itemId: string;
+    public itemHash: string;
+}
+
+export class StackableItem extends InventoryItem {
+    public stackSize: number;
+}
+
+export class GearItem extends InventoryItem {
+    public isEquipped: boolean;
+    public bucket: GearBucket;
+}
+
+export class WeaponItem extends GearItem {
+    public damageType: DamageType;
+}
+
+export enum InventoryItemTier {
+    Common,
+    Uncommon,
+    Rare,
+    Legendary,
+    Exotic
+}
+
+export enum GearBucket {
+    // Subclass
+    Subclass,
+
+    // Weapons
+    PrimaryWeapon,
+    SpecialWeapon,
+    HeavyWeapon,
+
+    // Armor
+    Helmet,
+    Gauntlets,
+    ChestArmor,
+    LegArmor,
+    ClassItem,
+
+    // Equipment
+    GhostShell,
+    Vehicle,
+    Ship,
+    ArmorShader,
+    Emblem
+}
+
+export enum InventoryItemType {
+    // Subclasses
+    TitanSubclass,
+    HunterSubclass,
+    WarlockSubclass,
+
+    // Generic inventory
+    Consumable,
+    Material,
+    Currency,
+
+    // Weapons
+    AutoRifle,
+    ScoutRifle,
+    HandCannon,
+    PulseRifle,
+    Shotgun,
+    FusionRifle,
+    SniperRifle,
+    MachineGun,
+    RocketLauncher,
+
+    // Armor
+    Helmet,
+    Gauntlet,
+    ChestArmor,
+    LegArmor,
+    ClassItem,
+
+    // Equipment
+    GhostShell,
+    Vehicle,
+    Ship,
+    ArmorShader,
+    Emblem
+}
+
+export enum DamageType {
+    // Murmur is a weapon, yet reports 'None' due to its
+    // variable damage type
+    None,
+    Kinetic,
+    Solar,
+    Arc,
+    Void
+}
---
+++ ManagerConsole/ManagerConsole/parser-utils.ts
@@ -0,0 +1,46 @@
+﻿import Inventory = require('./inventory-item');
+
+class ParserUtils {
+    public static parseInventoryItemTier(tierString: string): Inventory.InventoryItemTier {
+        // TODO: Implement
+        return Inventory.InventoryItemTier.Exotic;
+    }
+
+    public static parseGearBucket(bucketString: string): Inventory.GearBucket {
+        // TODO: Implement
+        return Inventory.GearBucket.ArmorShader;
+    }
+
+    public static parseInventoryItemType(typeString: string): Inventory.InventoryItemType {
+        // TODO: Implement
+        return Inventory.InventoryItemType.Currency;
+    }
+
+    public static parseDamageType(typeString: string): Inventory.DamageType {
+        // TODO: Implement
+        return Inventory.DamageType.Kinetic;
+    }
+
+
+    public static stringifyInventoryItemTier(tier: Inventory.InventoryItemTier): string {
+        // TODO: Implement
+        return "";
+    }
+
+    public static stringifyGearBucket(bucket: Inventory.GearBucket): string {
+        // TODO: Implement
+        return "";
+    }
+
+    public static stringifyInventoryItemType(type: Inventory.InventoryItemType): string {
+        // TODO: Implement
+        return "";
+    }
+
+    public static stringifyDamageType(type: Inventory.DamageType): string {
+        // TODO: Implement
+        return "";
+    }
+}
+
+export = ParserUtils;
