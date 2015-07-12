df8e7ae4b92a155f79fb8ef247a37081e6bf5408
Wasabi Fan <wasabifan@outlook.com>
2015-07-11 23:11:15 -0700

    Add command console infrastructure and test commands

--- ManagerConsole/ManagerConsole/app.ts
+++ ManagerConsole/ManagerConsole/app.ts
@@ -1,6 +1,7 @@
 ﻿import Vault = require('./bungie-api/vault-api');
 import Gear = require('./bungie-api/gear-api');
 import Configuration = require('./config-manager');
+import Console = require('./command-console');
 
 //Vault.getItems(function (items) {
 //    console.log(items);
@@ -12,3 +13,17 @@ import Configuration = require('./config-manager');
 
 Configuration.currentConfig.save();
 console.log('Saved configuration');
+
+var consoleConfig = new Console.CommandConsoleOptions();
+
+var commandB = new Console.Command('b', (...args: string[]) => {
+    console.log('B command run with args: ' + args);
+});
+
+var commandC = new Console.Command('c',(...args: string[]) => {
+    console.log('C command run with args: ' + args);
+});
+
+consoleConfig.commandRoot = new Console.Command('a', [commandB, commandC]);
+var cmdConsole = new Console.CommandConsole(consoleConfig);
+cmdConsole.start();
---
+++ ManagerConsole/ManagerConsole/command-console.ts
@@ -0,0 +1,55 @@
+﻿var sys = require('sys');
+
+export class CommandConsole {
+    private options: CommandConsoleOptions;
+
+    constructor(options: CommandConsoleOptions) {
+        this.options = options;
+    }
+
+    public start() {
+        process.stdin.addListener('data', data => {
+            var commandParts: string[] = data.toString().replace(/(\r\n|\n|\r)/gm, "").split(' ');
+
+            var currentCommand = this.options.commandRoot;
+            var subcommand;
+            for (var i = 0; i < commandParts.length && (subcommand = currentCommand.getSubcommand(commandParts[i + 1])) != null; i++) {
+                currentCommand = subcommand;
+            }
+
+            if (currentCommand.action != undefined) {
+                currentCommand.action.apply(this, commandParts.slice(i + 1));
+            }
+        });
+    }
+}
+
+export class CommandConsoleOptions {
+    public commandRoot: Command;
+}
+
+export class Command {
+    public name: string;
+    public action: (...args: string[]) => string | void;
+    public subcommands: Command[];
+
+    constructor(name: string, actionInfo: ((...args: string[]) => string | void) | Command[]) {
+        this.name = name;
+
+        if (typeof actionInfo === 'function') {
+            this.action = <(...args: string[]) => string | void> actionInfo;
+        }
+        else {
+            this.subcommands = <Command[]> actionInfo;
+        }
+    }
+
+    public getSubcommand(name: string) {
+        for (var i in this.subcommands) {
+            if (this.subcommands[i].name === name)
+                return this.subcommands[i];
+        }
+
+        return null;
+    }
+}
