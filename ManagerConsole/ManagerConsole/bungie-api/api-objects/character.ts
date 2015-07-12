f895d4d6b036e13e8bd0a44f2c9323761667b9ea
Wasabi Fan <wasabifan@outlook.com>
2015-07-11 20:22:45 -0700

    Add constructor for character class

--- ManagerConsole/ManagerConsole/bungie-api/api-objects/character.ts
+++ ManagerConsole/ManagerConsole/bungie-api/api-objects/character.ts
@@ -1,19 +1,23 @@
 ﻿export class Character {
-    public class: CharacterClass;
+    public characterClass: CharacterClass;
     public id: string;
+
+    constructor(id: string, characterClass: CharacterClass) {
+        this.characterClass = characterClass;
+        this.id = id;
+    }
 }
 
 export class AliasedCharacter extends Character {
     public alias: string;
 
+    constructor(id: string, characterClass: CharacterClass, alias: string) {
+        super(id, characterClass);
+        this.alias = alias;
+    }
+
     public static loadFromPlain(plainObj: any): AliasedCharacter {
-        var newCharacter = new AliasedCharacter();
-        newCharacter.alias = plainObj.alias;
-        newCharacter.id = plainObj.id;
-        newCharacter.class = plainObj.class;
-
-        return newCharacter;
-
+        return new AliasedCharacter(plainObj.id, plainObj.characterClass, plainObj.alias);
     }
 }
 
